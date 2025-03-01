dd6fd669911086d48868e6da7c7e6f12e81e08af
roselyn1234 <roselyn.adaya@aclcbutuan.edu.ph>
2025-03-01 09:28:55 +0800

    Add files via upload

---
+++ adayaday1.py
@@ -0,0 +1,8 @@
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+
+# Print the result
+print(f"The area of the rectangle is {area} square units.")
